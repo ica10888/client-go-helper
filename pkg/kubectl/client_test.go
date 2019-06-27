f5ebd5b11001a93f01e966097bc70f24c9796ffe
ica10888 <ica10888@gmail.com>
2019-06-27 22:48:26 +0800

    add client test

--- pkg/kubectl/client_test.go
+++ pkg/kubectl/client_test.go
@@ -2,8 +2,15 @@ package kubectl
 
 import "testing"
 
-	func Test_InitClient(t *testing.T) {
+func Test_InitClient(t *testing.T) {
 		for i := 0; i < 10; i++ {
 			go InitClient()
 		}
+}
+
+
+func Test_InitRestClient(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go InitRestClient()
+	}
 }
