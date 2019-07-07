4489fd297505a3b1756f422d09557e8a1f7d502c
ica10888 <ica10888@gmail.com>
2019-07-07 22:41:09 +0800

    fix

--- pkg/test/generating_func_test.go
+++ pkg/test/generating_func_test.go
@@ -81,3 +81,80 @@ func (i *%s) Delete (opts *v1.DeleteOptions) (error) {
 
 
 }
+
+
+func TestWriteListCode(t *testing.T)  {
+
+	clientset, _ := kubectl.InitClient()
+
+	//输出转输入
+	writeListCode(GetFunctionName2(clientset.BatchV1beta1()),"BatchV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.AuditregistrationV1alpha1()),"AuditregistrationV1alpha1")
+
+	writeListCode(GetFunctionName2(clientset.AdmissionregistrationV1beta1()),"AdmissionregistrationV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.BatchV1()),"BatchV1")
+
+	writeListCode(GetFunctionName2(clientset.CertificatesV1beta1()),"CertificatesV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.RbacV1()),"RbacV1")
+
+	writeListCode(GetFunctionName2(clientset.CoordinationV1beta1()),"CoordinationV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.NetworkingV1()),"NetworkingV1")
+
+	writeListCode(GetFunctionName2(clientset.AppsV1()),"AppsV1")
+
+	writeListCode(GetFunctionName2(clientset.AutoscalingV2beta2()),"AutoscalingV2beta2")
+
+	writeListCode(GetFunctionName2(clientset.PolicyV1beta1()),"PolicyV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.AuthorizationV1()),"AuthorizationV1")
+
+	writeListCode(GetFunctionName2(clientset.CoreV1()),"CoreV1")
+
+	writeListCode(GetFunctionName2(clientset.SettingsV1alpha1()),"SettingsV1alpha1")
+
+	writeListCode(GetFunctionName2(clientset.AdmissionregistrationV1alpha1()),"AdmissionregistrationV1alpha1")
+
+	writeListCode(GetFunctionName2(clientset.AuthenticationV1()),"AuthenticationV1")
+
+	writeListCode(GetFunctionName2(clientset.ExtensionsV1beta1()),"ExtensionsV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.SchedulingV1beta1()),"SchedulingV1beta1")
+
+	writeListCode(GetFunctionName2(clientset.StorageV1()),"StorageV1")
+}
+
+
+func writeListCode (args map[string]bool,api string){
+	for  v,vbool := range args {
+		if v!= "" {
+			var vs = v + "s"
+			if  v[len(v)-1:]== "s"  {
+				vs = v + "es"
+			}
+			if v[len(v)-1:]== "y" {
+				vs = v[:len(v)-1] + "ies"
+			}
+
+			var ns = "i.Namespace"
+			if vbool == false {
+				ns = ""
+			}
+			fmt.Printf(`
+func (i *%s) GetAll(opts *v1.ListOptions) ([]%s.%s, error) {
+	%sList, e := clientset.%s().%s(%s).List(*opts)
+	if e != nil {
+		return nil,e
+	}
+	return %sList.Items,nil
+}
+
+`, v, lcfirst(api), v,lcfirst(v) , api,vs, ns,lcfirst(v))
+		}
+	}
+
+
+}
