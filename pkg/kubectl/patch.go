1006fdece68de4ffe1f836cac0486349e9610cf4
ica10888 <ica10888@gmail.com>
2019-07-28 22:08:14 +0800

    add panch

---
+++ pkg/kubectl/patch.go
@@ -0,0 +1,17 @@
+package kubectl
+
+import (
+	batchV1beta1 "k8s.io/api/batch/v1beta1"
+	types "k8s.io/apimachinery/pkg/types")
+
+func (i *CronJob) Pench (pt *types.PatchType) (batchV1beta1.CronJob,error) {
+	var clientset, err  = InitClient()
+	if err != nil {
+		return batchV1beta1.CronJob{},err
+	}
+	cronJob , err := clientset.BatchV1beta1().CronJobs(i.Namespace).Patch(i.Name,*pt,"")
+	if err != nil {
+		return batchV1beta1.CronJob{},err
+	}
+	return *cronJob,nil
+}
