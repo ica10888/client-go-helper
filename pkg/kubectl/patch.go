fe62f67d57caa0ed52d74fae992319264087727c
ica10888 <ica10888@gmail.com>
2019-08-07 22:44:51 +0800

    impl patch

--- pkg/kubectl/patch.go
+++ pkg/kubectl/patch.go
@@ -4,12 +4,12 @@ import (
 	batchV1beta1 "k8s.io/api/batch/v1beta1"
 	types "k8s.io/apimachinery/pkg/types")
 
-func (i *CronJob) Patch (pt *types.PatchType) (batchV1beta1.CronJob,error) {
+func (i *CronJob) Patch (data string,pt *types.PatchType) (batchV1beta1.CronJob,error) {
 	var clientset, err  = InitClient()
 	if err != nil {
 		return batchV1beta1.CronJob{},err
 	}
-	cronJob , err := clientset.BatchV1beta1().CronJobs(i.Namespace).Patch(i.Name,*pt,nil,"")
+	cronJob , err := clientset.BatchV1beta1().CronJobs(i.Namespace).Patch(i.Name,*pt,[]byte(data))
 	if err != nil {
 		return batchV1beta1.CronJob{},err
 	}
